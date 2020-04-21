db0e04ad152132265f8074ca67bd83efe3310368
Aleksandar Dinic <a.dinic@icloud.com>
2020-04-21 12:54:36 +0200

    30 Days of Code - Day 05 Loops

---
+++ 30 Days of Code/Solutions/Day 05 Loops.swift
@@ -0,0 +1,18 @@
+//
+//  Day 05 Loops.swift
+//  HackerRank-Solutions/30 Days of Code
+//
+//  Created by Aleksandar Dinic on 21/04/2020.
+//  Copyright © 2020 Aleksandar Dinic. All rights reserved.
+//
+
+/// Source: https://www.hackerrank.com/challenges/30-loops/problem
+
+import Foundation
+
+let n = Int(readLine()!)!
+
+/// Prints first 10 multiples of **n**
+for i in 1...10 {
+    print("\(n) x \(i) = \(n * i)")
+}
